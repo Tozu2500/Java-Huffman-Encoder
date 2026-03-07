450da645a417ab98a4048e62bafd0e756fa3c553
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-03-08 01:43:34 +0200

    First commit

---
+++ HuffmanEncoder/src/huffman/Main.java
@@ -0,0 +1,24 @@
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Enable system-wide anti-aliasing
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
+        System.setProperty("sun.java2d.opengl", "true");
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // Use system look and feel as a base and then override
+                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            } catch (Exception ignored) {}
+            AppTheme.applyGlobalDefaults();
+            MainWindow win = new MainWindow();
+            win.setVisible(true);
+        });
+    }
+
+}
