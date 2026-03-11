f1ba7849c5ac4fe312a9fd87d10e2fc0bdd82c8e
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-03-11 22:13:07 +0200

    First component commit

---
+++ HuffmanEncoder/src/huffman/ui/components/SlimScrollBarUI.java
@@ -0,0 +1,47 @@
+package huffman.ui.components;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
+import huffman.ui.theme.AppTheme;
+
+public class SlimScrollBarUI extends BasicScrollBarUI {
+
+    @Override
+    protected void configureScrollBarColors() {
+        thumbColor = AppTheme.BG_HOVER;
+        trackColor = AppTheme.BG_BASE;
+    }
+
+    @Override
+    protected JButton createDecreaseButton(int o) {
+        return zeroButton();
+    }
+
+    @Override
+    protected JButton createIncreaseButton(int o) {
+        return zeroButton();
+    }
+
+    @Override
+    protected void paintThumb(Graphics g, JComponent c, Rectangle r) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(thumbColor);
+        g2.fillRoundRect(r.x + 2, r.y + 2, r.width - 4, r.height - 4, 6, 6);
+        g2.dispose();
+    }
+
+    @Override
+    protected void paintTrack(Graphics g, JComponent c, Rectangle r) {
+        g.setColor(trackColor);
+        g.fillRect(r.x, r.y, r.width, r.height);
+    }
+
+}
