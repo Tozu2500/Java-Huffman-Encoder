47478d10c59edf12aefe374909f63008ec0dc8d6
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-03-14 01:31:19 +0200

    Thought
    Thinking of creating a full easy to use Swing framework for Java Desktop applications

--- HuffmanEncoder/src/huffman/ui/components/DarkTextArea.java
+++ HuffmanEncoder/src/huffman/ui/components/DarkTextArea.java
@@ -1,23 +1,13 @@
 package huffman.ui.components;
 
-import javax.swing.JTextArea;
+import huffman.ui.theme.AppTheme;
+import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-
-import huffman.ui.theme.AppTheme;
-
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Insets;
-import java.awt.RenderingHints;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-
-import javax.swing.BorderFactory;
-import javax.swing.JScrollPane;
+import java.awt.*;
+import java.awt.event.*;
 
 public class DarkTextArea extends JTextArea {
-
     private String placeholder = "";
     private boolean showPlaceholder = true;
 
@@ -31,49 +21,28 @@ public class DarkTextArea extends JTextArea {
         setWrapStyleWord(true);
         setOpaque(true);
         setBorder(BorderFactory.createCompoundBorder(
-            new AppTheme.RoundedBorder(AppTheme.BORDER, AppTheme.RADIUS, 1),
-            BorderFactory.createEmptyBorder(10, 12, 10, 12)
-        ));
+                new AppTheme.RoundedBorder(AppTheme.BORDER, AppTheme.RADIUS, 1),
+                BorderFactory.createEmptyBorder(10, 12, 10, 12)));
 
         getDocument().addDocumentListener(new DocumentListener() {
-            void update() {
-                showPlaceholder = getText().isEmpty();
-                repaint();
-            }
-
-            public void insertUpdate(DocumentEvent e) {
-                update();
-            }
-
-            public void removeUpdate(DocumentEvent e) {
-                update();
-            }
-
-            public void changedUpdate(DocumentEvent e) {
-                update();
-            }
+            void update() { showPlaceholder = getText().isEmpty(); repaint(); }
+            public void insertUpdate(DocumentEvent e) { update(); }
+            public void removeUpdate(DocumentEvent e) { update(); }
+            public void changedUpdate(DocumentEvent e) { update(); }
         });
 
         addFocusListener(new FocusAdapter() {
-            public void focusGained(FocusEvent e) {
-                repaint();
-            }
-
-            public void focusLost(FocusEvent e) {
-                repaint();
-            }
+            public void focusGained(FocusEvent e) { repaint(); }
+            public void focusLost(FocusEvent e)   { repaint(); }
         });
     }
 
-    public void setPlaceholder(String s) {
-        this.placeholder = p;
-        repaint();
-    }
+    public void setPlaceholder(String p) { this.placeholder = p; repaint(); }
 
-    @Override
-    protected void paintComponent(Graphics g) {
+    @Override protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (showPlaceholder && !placeholder.isEmpty()) {
+            Graphics2D g2 = (Graphics2D) g.create();
             g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
             g2.setColor(AppTheme.TEXT_MUTED);
             g2.setFont(getFont().deriveFont(Font.ITALIC));
@@ -85,8 +54,8 @@ public class DarkTextArea extends JTextArea {
 
     public JScrollPane inScrollPane() {
         JScrollPane sp = new JScrollPane(this,
-            JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         sp.setOpaque(false);
         sp.getViewport().setOpaque(false);
         sp.setBorder(BorderFactory.createEmptyBorder());
